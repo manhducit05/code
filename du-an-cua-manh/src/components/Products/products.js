f8dbaea76a98f09d1a3ff4decd3de1cd8f0e6122
Manh Duc <manhducit05.com>
2023-11-01 10:38:11 +0700

    tao main

---
+++ du-an-cua-manh/src/components/Products/products.js
@@ -0,0 +1,44 @@
+import {useState, useEffect} from 'react'
+function Products(){
+    const [loading,setLoading] = useState(true)
+    const [data, setData] = useState([]);
+    useEffect(()=>{
+        const fetchApi = ()=>{
+            fetch("https://dummyjson.com/products")
+            .then (res=>res.json())
+            .then(data=>{
+                setData(data.products);
+            })
+        } 
+        setTimeout(()=>{
+            setLoading(false);
+            fetchApi();
+        },3000);
+    },[data])
+    
+    return ( 
+        <>
+        {
+            loading?(<>Chờ tí...</>):(
+                <>
+                <div className='listProducts'>
+                {
+                    data.map((item,_)=>(
+                        <div className='item'>
+                            <div className='name'>{item.title}</div>
+                            <div className='img'><img src={item.thumbnail}/></div>
+                            <div className='price'>{item.price}</div>
+                        </div>
+                        )
+                    )
+                }
+            </div></>
+
+            )
+        
+        }
+        </>
+
+    )
+}
+export default Products 
